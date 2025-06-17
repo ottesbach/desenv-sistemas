c4eeb1ac86e69b9f3950dd087d4150472f364658
Beatriz Ottesbach <bottesbachsanches@gmail.com>
2025-06-17 09:52:58 -0300

    Trabalhando com variáveis

--- backend/logicaProgramacao/01_variaveis.py
+++ backend/logicaProgramacao/01_variaveis.py
@@ -7,3 +7,27 @@ print("Nome:", nome)
 print("Idade:", idade)
 print("Altura", altura)
 print("Tem carteira:", tem_carteira)
+
+escola = "particular"
+trabalho = "infantil"
+
+print("escola:", escola)
+print("trabalho:", trabalho)
+
+numero = 18
+velocidade = 1000
+
+print("numero:", numero)
+print("velocidade:", velocidade)
+
+peso = 80.51
+temperatura = 23.5
+
+print("peso:", peso)
+print("temperatura:", temperatura)
+
+rafael_irresponsavel = True
+e_legal = False
+
+print("rafael irresponsavel?:",rafael_irresponsavel)
+print("e legal:", e_legal) 
