055382754b5b2aa927490a47fd9de5c4d3728e9f
Beatriz <bottesbachsanches@gmail.com>
2025-06-23 09:11:14 -0300

    trabalhando com condicionais

---
+++ backend/logicaProgramacao/02_condicionais.py
@@ -0,0 +1,8 @@
+nota = int(input("Digite sua nota"))
+
+if (nota >= 80):
+    print ("Parabéns")
+elif (nota < 80 and nota > 60):
+    print ("Pode melhorar")
+elif (nota < 60):
+    print ("Recuperação") 
