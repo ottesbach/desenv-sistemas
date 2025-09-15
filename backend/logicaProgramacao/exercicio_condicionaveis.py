3736f12b8b1a0ad848fbc497de3bc98c7e9b767a
beatriz <bottesbachsanches@gmail.com>
2025-09-15 08:53:20 -0300

    exerciciosavaliativo.txt

---
+++ backend/logicaProgramacao/exercicio_condicionaveis.py
@@ -0,0 +1,11 @@
+#03 explique porque quando tenha mais de um atributo(variável), torna-se dificil/complicado o uso de arquivos txt para guardar informações
+
+Quando tem mais de um atributo (variável) para guardar informações de um sistema (como dados de clientes, produtos, usuários), o uso de arquivos.
+txt simples se torna complicado por: 
+
+Arquivos txt é praticamente texto, sem uma estrutura, como tabelas ou campos. Quando tem uma informação por linha, é fácil de entender, mas quando tem vários atributos por item, 
+é necessário:
+
+Escolher um separador (como vírgula, ponto e vírgula e tabulação).
+
+Garantir que o separador não apareça nos próprios dados. 
